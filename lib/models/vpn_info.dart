607545c422df13be5f562ce84dd74d05dc6c167c
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2024-07-04 22:55:21 +0600

    home controller intiazize

--- lib/models/vpn_info.dart
+++ lib/models/vpn_info.dart
@@ -43,4 +43,7 @@ class VpnInfo {
 
     return jsonData;
   }
+
+
+
 }
--- lib/views/app_preferences/app_preferences.dart
+++ lib/views/app_preferences/app_preferences.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:my_vpn/models/vpn_info.dart';
 
 class AppPreferences {
   static late Box boxOfData;
@@ -8,4 +11,27 @@ class AppPreferences {
     boxOfData = await Hive.openBox("data");
   }
 
+  //for saving single selected vpn details
+
+  static VpnInfo get vpnInfoObj =>
+      VpnInfo.fromJson(jsonDecode(boxOfData.get("vpn") ?? '{}'));
+
+  static set vpnInfoObj(VpnInfo value) =>
+      boxOfData.put('vpn', jsonEncode(value));
+
+  //for saving all vpn server details
+  static List<VpnInfo> get vpnList {
+    List<VpnInfo> tempVpnList = [];
+
+    final dataVpn = jsonDecode(boxOfData.get('vpnList') ?? '[]');
+
+    for (var data in dataVpn) {
+      tempVpnList.add(VpnInfo.fromJson(data));
+    }
+
+    return tempVpnList;
+  }
+
+  static set vpnList(List<VpnInfo> valueList) =>
+      boxOfData.put('vpnList', jsonEncode(valueList));
 }
